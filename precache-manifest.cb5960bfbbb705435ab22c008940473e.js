a5b4724f6c805bd699ade62257d35f9ddc4af02c
Baimyrza <baimurza.lesbek@gmail.com>
2022-12-24 13:32:15 +0600

    Updates

---
+++ precache-manifest.cb5960bfbbb705435ab22c008940473e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444f91acba937bb67e522619519bb35e",
+    "url": "/react-braintest/index.html"
+  },
+  {
+    "revision": "589dc22b7619c6acb901",
+    "url": "/react-braintest/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "7be6be9add94e799d7e9",
+    "url": "/react-braintest/static/css/main.1862cede.chunk.css"
+  },
+  {
+    "revision": "589dc22b7619c6acb901",
+    "url": "/react-braintest/static/js/2.31bb0ad6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-braintest/static/js/2.31bb0ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be6be9add94e799d7e9",
+    "url": "/react-braintest/static/js/main.1021d39a.chunk.js"
+  },
+  {
+    "revision": "7f77e6974f0aa128ce2d",
+    "url": "/react-braintest/static/js/runtime-main.9e2f74d0.js"
+  }
+]);
